49f0c43b75329f65eb1b61a81f2a3f3dc328b208
Roman Bondaruk <26bondaruk@gmail.com>
2022-06-02 20:44:40 +0300

    Added CreateFile

--- HelloW/HelloW/main.cpp
+++ HelloW/HelloW/main.cpp
@@ -1,7 +1,23 @@
 #include<iostream>
+#include<windows.h>
 
 int main()
 {
 	std::cout << "Hello git"<<std::endl;
+	__asm
+	{
+		mov eax, ebx;
+	}
+
+	HANDLE handle = CreateFile(L"1.txt",
+		GENERIC_ALL,
+		0,
+		0,
+		CREATE_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL,
+		0);
+
+	CloseHandle(handle);
+
 	return EXIT_SUCCESS;
 }
